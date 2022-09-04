0c80268c1d0fed2a99632bdf3541d450b2441bfd
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-09-04 20:02:01 -0300

    Por hoje é só

--- portifolio/src/pages/Login.jsx
+++ portifolio/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { createUser } from '../services/userAPI';
 import MsgLoading from '../components/MsgLoading';
 import '../css/Login.css';
@@ -8,37 +8,44 @@ class Login extends React.Component {
   constructor() {
     super();
     this.state = {
-      btnLogin: '',
       login: false,
-      email: '',
+      name: '',
       next: false,
-      password: '',
     };
   }
 
-  botãoEntrar = async () => {
-    const { email } = this.state;
+  botaoEntrar = async () => {
+    const { name } = this.state;
     this.setState({ loading: true });
-    await createUser({ email }).then((event) => {
+    await createUser({ name }).then((event) => {
       if (event) {
         this.setState({ next: true });
       }
     });
   };
 
+  aprovado = () => {
+    const { name } = this.state;
+
+    const minLength = 2;
+    const outraInfo = name.length >= minLength;
+
+    return !outraInfo;
+  };
+
   trocarInput = ({ target }) => {
     const { value } = target;
     this.setState({
-      email: value,
+      name: value,
     });
   };
 
   render() {
-    const { email, loading, next } = this.state;
+    const { name, loading, next } = this.state;
     return (
       <div className='container' id='container'>
         {loading && <MsgLoading />}
-        {next && <Redirect to='/welcome' />}
+        {next && <Navigate to='/welcome' />}
         <div className='form-container sign-in-container'>
           <form>
             <h1>Login</h1>
@@ -46,16 +53,15 @@ class Login extends React.Component {
               Coloque algo como: <br /> qualquer@coisa.com
             </span>
             <input
-              type='email'
-              placeholder='Email'
+              type='text'
               name='name'
               id='name'
-              value={email}
+              value={name}
               onChange={this.trocarInput}
             />
             <input type='password' placeholder='Password' />
             <span>Nada será salvo</span>
-            <button onClick={this.botãoEntrar} disabled={this.aprovado()}>
+            <button onClick={this.botaoEntrar} disabled={this.aprovado()}>
               Entrar
             </button>
           </form>
@@ -63,10 +69,14 @@ class Login extends React.Component {
 
         <div className='overlay-container'>
           <div className='overlay'>
-            <div className='irDireto irDireto-right'>
+            <div className='overlay-panel overlay-right'>
               <h1>Ir Direto</h1>
               <p>Se preferir pode entrar direto</p>
-              <button className='ghost' id='signUp'>
+              <button
+                onClick={this.botaoEntrar}
+                disabled={this.aprovado()}
+                className='ghost'
+                id='signUp'>
                 Entrar
               </button>
             </div>
