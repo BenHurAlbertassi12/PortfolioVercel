132997bbbd9d2dfad47c7476c86623e06b5d4216
BenHur Albertassi da Silva <benhuralber@yahoo.com.br>
2022-12-21 18:55:38 -0300

    FIX: css "Sobre Mim"

--- portifolio/src/pages/SobreMim.jsx
+++ portifolio/src/pages/SobreMim.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import '../css/Sobre.css'
+
+import EuUnicaFotoBonita from '../image/EuUnicaFotoBonita.jpg';
+import EstudioAdore from '../image/EstudioAdore.jpg';
+import Teatro from '../image/Teatro.jpg';
 
 class SobreMim extends React.Component {
   render() {
@@ -12,59 +17,82 @@ class SobreMim extends React.Component {
             <h1>Ben-Hur Albertassi</h1>
             <h2>Desenvolvedor Full-stack</h2>
           </div>
-          <div>
-            <div className='sobre'>
-              <h3>Sobre Mim</h3>
-              <p>
-                Sou Apaixonado pela pela evolução humana e por toda a tecnologia
-                criada, acredito estarmos vivendo uma das melhores épocas em
-                relação ao desenvolvimento tecnológico.
-              </p>
-              <br />
-              <p>
-                Me tornei fascinado por programação em 2007 quando existiu um
-                “boom” no Brasil na criação de servidores de jogos, onde meu
-                primeiro contato com programação foi com o jogo “Tibia” mais
-                tarde com “Ragnarok” e “Mu”, onde tive servidores constantemente
-                atualizados e melhorados por mim e meu irmão
-              </p>
-              <br />
-              <p>
-                Em 2013 cometi o pequeno erro de entrar para o curso errado de
-                Engenharia de Controle e Automação onde percebi que era
-                realmente com programação que gostaria de trabalhar, tranquei o
-                curso no em 2015 pois tentaria entrar para a CEDERJ no Rio de
-                Janeiro para o curso de TI (2016 e 2017), como estava muito
-                atarefado com meu emprego acabei não conseguindo estudar, sendo
-                assim não fui aprovado
-              </p>
-            </div>
-            <br />
-            <div className='prof'>
-              <h3>Profissional</h3>
-              <p>
-                Consegui meu primeiro emprego aos 12 anos em um escritório de
-                advocacia, ao completar 18 anos fui convidado para trabalhar na
-                Radio88fm onde permaneci por 12 anos, meu primeiro ano foi como
-                Auxiliar de escritório e posteriormente como produtor comercial,
-                o que me trouxe muita proximidade com os clientes pois tinha que
-                entender o que eles gostariam para determinada produção. Também
-                trabalhei nos estúdios do ar e no setor comercial
-              </p>
-            </div>
-            <br />
-            <div className='lazer'>
-              <h3>Lazer</h3>
-              <p>
-                Como um bom nerd eu adoro filmes e series/animes
-                <br />
-                Codar
-                <br />
-                Gatos e cachorros (um pouco mais de gatos)
-                <br />
-                Volei e Patins
-              </p>
-            </div>
+          <div className='sobre'>
+            <section className='sobreMim'>
+              <img
+                src={EuUnicaFotoBonita}
+                alt='BenHur Albertassi fazendo foto de modelo'
+                className='fotoBonita'
+                />
+              <div className='textSobre'>
+                <h3>Sobre Mim</h3>
+                <p>
+                  Sou Apaixonado pela pela evolução humana e por toda a
+                  tecnologia criada, acredito estarmos vivendo uma das melhores
+                  épocas em relação ao desenvolvimento tecnológico.
+                  <br />
+                  Me tornei fascinado por programação em 2007 quando existiu um
+                  “boom” no Brasil na criação de servidores de jogos, onde meu
+                  primeiro contato com programação foi com o jogo “Tibia” mais
+                  tarde com “Ragnarok” e “Mu”, onde tive servidores
+                  constantemente atualizados e melhorados por mim e meu irmão
+                  <br />
+                  Em 2013 cometi o pequeno erro de entrar para o curso errado de
+                  Engenharia de Controle e Automação onde percebi que era
+                  realmente com programação que gostaria de trabalhar, tranquei
+                  o curso no em 2015 pois tentaria entrar para a CEDERJ no Rio
+                  de Janeiro para o curso de TI (2016 e 2017), como estava muito
+                  atarefado com meu emprego acabei não conseguindo estudar,
+                  sendo assim não fui aprovado
+                </p>
+              </div>
+            </section>
+            <hr />
+            <section className='prof'>
+              <div className='textSobre'>
+                <h3>Profissional</h3>
+                <p>
+                  Consegui meu primeiro emprego aos 12 anos em um escritório de
+                  advocacia, ao completar 18 anos fui convidado para trabalhar
+                  na Radio88fm onde permaneci por 12 anos, meu primeiro ano foi
+                  como Auxiliar de escritório e posteriormente como produtor
+                  comercial, o que me trouxe muita proximidade com os clientes
+                  pois tinha que entender o que eles gostariam para determinada
+                  produção. Também trabalhei nos estúdios do ar e no setor
+                  comercial
+                </p>
+              </div>
+                <img
+                  src={EstudioAdore}
+                  alt='Estudios Radio Adore Fm 2021'
+                  className='fotoEstudio'
+                />
+            </section>
+            <hr />
+            <section className='lazer'>
+              <img
+                src={Teatro}
+                alt='Troupe de teatro em 2012'
+                className='fotoTeatro'
+              />
+              <div className='textSobre'>
+              <h3>Curiosidades</h3>
+                <p>
+                  Como um bom nerd eu adoro:
+                  <br></br>
+                  Filmes e series/animes
+                  <br />
+                  Musica, muita musica
+                  <br />
+                  Codar
+                  <br />
+                  Gatos e cachorros (um pouco mais de gatos)
+                  <br />
+                  Volei e Patins.
+                </p>
+                <p>Ja fiz curso de dublagem e adoro atuar</p>
+              </div>
+            </section>
           </div>
         </body>
         <Footer />
