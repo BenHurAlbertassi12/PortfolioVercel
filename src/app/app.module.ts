3e9d070f2535c15099f39db5ee6a5d41a7c807d4
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-03 16:42:40 -0300

    eita lasqueira

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderBarComponent } from './components/header-bar/header-bar.component';
-import { TitleComponent } from './components/title/title.component';
-import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallCardComponent } from './components/small-card/small-card.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ContentComponent } from './pages/content/content.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfoilComponent } from './components/portfoil/portfoil.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TestemonialsComponent } from './components/testemonials/testemonials.component';
+
+// import { MatIconModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderBarComponent,
-    TitleComponent,
-    BigCardComponent,
-    SmallCardComponent,
     HomeComponent,
-    ContentComponent
+    HeaderBarComponent,
+    AboutComponent,
+    PortfoilComponent,
+    ContactComponent,
+    TestemonialsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    // MatIconModule,
+    BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/portfoil/portfoil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfoil',
+  templateUrl: './portfoil.component.html',
+  styleUrls: ['./portfoil.component.css']
+})
+export class PortfoilComponent {
+
+}
