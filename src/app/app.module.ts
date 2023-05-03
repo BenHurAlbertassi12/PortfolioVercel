bcd96c5298e8592d774c3174c9d4112ba5a59bbd
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-03 13:28:29 -0300

    new project

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './pages/content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderBarComponent,
+    TitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
