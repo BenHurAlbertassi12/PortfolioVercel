585c181b8c54a347688aa1d1f8223e10fce7b543
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-07 23:31:39 -0300

    data

---
+++ src/app/data/about.ts
@@ -0,0 +1,43 @@
+export const benhur = [
+  {
+    name: 'Ben-Hur Albertassi',
+    function: 'Desenvolverdor Full Stack',
+    descrition: `
+        Procurando por um desenvolvedor full-stack habilidoso em uma ampla variedade de linguagens e tecnologias populares? Eu sou o candidato certo para você!
+
+        Com experiência junior em Java, JavaScript, Node.Js, React, GIT, GitHub, CSS, TypeScript, MongoDB, Redux, Hooks, REST e HTML, Python e Angular, posso assumir projetos desafiadores de front-end ou back-end.
+
+        Meu portfólio impressionante inclui projetos como Spongefy, PixelArt e StarWars Planets. Minha meta de carreira é atingir a excelência e me tornar uma referência na área, tenho habilidades de comunicação e trabalho em equipe para ajudar a alcançar essa meta.
+
+        Estou acostumado a lidar com prazos apertados e tarefas complexas, graças à minha experiência anterior, tenho conhecimento em metodologias de desenvolvimento de software, como Trello e Scrum.
+
+        Não procure mais se você precisa de um desenvolvedor full-stack confiável e talentoso para sua equipe!`,
+    courses: [
+      {
+        id: 1,
+        name: 'Desenvolvimento Web Full-stack',
+        course: 'Trybe',
+      },
+      {
+        id: 2,
+        name: 'Orange Tech + | BackEnd',
+        course: 'Digital Innovation One',
+      },
+      {
+        id: 3,
+        name: 'Potência Tech Angular Developer',
+        course: 'Digital Innovation One',
+      },
+      {
+        id: 4,
+        name: 'TypeScript Fullstack Developer',
+        course: 'Digital Innovation One',
+      },
+      {
+        id: 5,
+        name: 'Linux do Zero',
+        course: 'Digital Innovation One',
+      },
+    ],
+  },
+];
