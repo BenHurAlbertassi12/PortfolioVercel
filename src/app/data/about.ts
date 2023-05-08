60e06026eadd39c37798da5dc22444b332bc8bea
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-07 23:46:58 -0300

    Courses

--- src/app/data/about.ts
+++ src/app/data/about.ts
@@ -11,32 +11,82 @@ export const benhur = [
 
         Estou acostumado a lidar com prazos apertados e tarefas complexas, graças à minha experiência anterior, tenho conhecimento em metodologias de desenvolvimento de software, como Trello e Scrum.
 
-        Não procure mais se você precisa de um desenvolvedor full-stack confiável e talentoso para sua equipe!`,
+        Não procure mais se você precisa de um desenvolvedor full-stack confiável e talentoso para sua equipe!
+        `,
     courses: [
       {
         id: 1,
         name: 'Desenvolvimento Web Full-stack',
         course: 'Trybe',
+            stacks: [{
+                fundamentos: {
+                
+                },
+                frontend: {
+                    
+                },
+                backend: {
+                    
+                },
+                cienciacomputacao: {
+                    
+                },
+        }],
       },
       {
         id: 2,
         name: 'Orange Tech + | BackEnd',
         course: 'Digital Innovation One',
+        stacks: [
+          'Java',
+          'SpringBoot',
+          'Dependências e Build',
+          'POO',
+          'Exceções em Java',
+          'Collections Java',
+          'Métodos',
+          'Sintaxe Java',
+        ],
       },
       {
         id: 3,
         name: 'Potência Tech Angular Developer',
         course: 'Digital Innovation One',
+        stacks: [
+          'JavaScript',
+          'CSS',
+          'APIs',
+          'Angular',
+          'Modulos',
+          'LifeCycle',
+          'Hooks',
+          'Componentes',
+          'TypeScript',
+        ],
       },
       {
         id: 4,
         name: 'TypeScript Fullstack Developer',
         course: 'Digital Innovation One',
+        stacks: [
+          'TypeScript',
+          'Node', 'JWT',
+          'React', 'APIs',
+          'POO'
+          ],
       },
       {
         id: 5,
         name: 'Linux do Zero',
         course: 'Digital Innovation One',
+        stacks: [
+          'Docker',
+          'Linux',
+          'Servidores',
+          'Pacotes',
+          'Discos',
+          'Usuarios',
+        ],
       },
     ],
   },
