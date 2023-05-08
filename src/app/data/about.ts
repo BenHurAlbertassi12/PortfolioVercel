ac6a0c260cd2d8b749f78c0aa32ff60143e6937c
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-08 01:28:48 -0300

    Data

--- src/app/data/about.ts
+++ src/app/data/about.ts
@@ -29,7 +29,8 @@ export const benhur = [
                   'HTML e CSS',
                   'Testes Unitários',
                 ],
-                imagem: '',
+                imagem:
+                  'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
               },
             ],
 
@@ -46,6 +47,8 @@ export const benhur = [
                   'Hooks',
                   'Metodologias Ageis',
                 ],
+                imagem:
+                  'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
               },
             ],
 
@@ -53,7 +56,7 @@ export const benhur = [
               {
                 stacks: [
                   'Docker',
-                  'banco de dados SQL e NoSQL',
+                  'Banco de dados SQL e NoSQL',
                   'Node.js e Express',
                   'Mocha',
                   'Chai e Sinon',
@@ -65,9 +68,11 @@ export const benhur = [
                   'TypeScript',
                   'POO',
                   'SOLID',
-                  'conceitos de CI/CD com Github Actions',
+                  'Conceitos de CI/CD com Github Actions',
+                  'MongoDB',
                 ],
-                imagem: '',
+                imagem:
+                  'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
               },
             ],
           },
@@ -87,6 +92,8 @@ export const benhur = [
           'Métodos',
           'Sintaxe Java',
         ],
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 3,
@@ -103,12 +110,16 @@ export const benhur = [
           'Componentes',
           'TypeScript',
         ],
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 4,
         name: 'TypeScript Fullstack Developer',
         course: 'Digital Innovation One',
         stacks: ['TypeScript', 'Node', 'JWT', 'React', 'APIs', 'POO'],
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 5,
@@ -122,6 +133,8 @@ export const benhur = [
           'Discos',
           'Usuarios',
         ],
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
     ],
     project: [
@@ -129,79 +142,171 @@ export const benhur = [
         id: 1,
         name: 'Portifólio',
         stacks: ['Angular', 'JavaScript', 'TypeScript', 'HTML & CSS'],
+        deploy: '',
         reposit: 'https://github.com/BenHurAlbertassi12/PortfolioVercel',
-        imagem: '',
+        imagem: '../../../../assets/icons/icons8-ok-64.png',
       },
       {
         id: 2,
-        name: '',
-        stacks: ['', ''],
-        reposit: '',
-        imagem: '',
+        name: 'App de Receitas',
+        stacks: ['React', 'HMLT & CSS', 'Context API', 'Hooks'],
+        deploy: 'https://github.com/BenHurAlbertassi12/receitasApp',
+        reposit: 'https://github.com/BenHurAlbertassi12/receitasApp',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 3,
-        name: '',
-        stacks: ['', ''],
-        reposit: '',
-        imagem: '',
+        name: 'Star Wars System',
+        stacks: [
+          'JavaScript',
+          'HTML',
+          'BootsTrap',
+          'React',
+          'Hooks',
+          'Context API',
+        ],
+        deploy: 'https://starwars-plants.vercel.app',
+        reposit: 'https://github.com/BenHurAlbertassi12/StarWarsSystem',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 4,
-        name: '',
-        stacks: ['', ''],
-        reposit: '',
-        imagem: '',
+        name: 'Tryvia',
+        stacks: ['JavaScript', 'Redux', 'React', 'HTML & CSS'],
+        deploy:
+          'https://github.com/BenHurAlbertassi12/tryviaRedux/blob/main/src/pages/Game.js',
+        reposit:
+          'https://github.com/BenHurAlbertassi12/tryviaRedux/blob/main/src/pages/Game.js',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 5,
-        name: '',
-        stacks: ['', ''],
-        reposit: '',
-        imagem: '',
+        name: 'Trybers&dragons',
+        stacks: ['TypeScrip', 'POO'],
+        deploy: 'https://github.com/BenHurAlbertassi12/Trybers-and-dragons',
+        reposit: 'https://github.com/BenHurAlbertassi12/Trybers-and-dragons',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 6,
-        name: '',
-        stacks: ['', ''],
-        reposit: '',
-        imagem: '',
+        name: 'Blogs API',
+        stacks: ['Docker', 'Node', 'Express', 'Sequelize', 'JWT', 'Rest'],
+        deploy: 'https://github.com/BenHurAlbertassi12/Blogs-Api',
+        reposit: 'https://github.com/BenHurAlbertassi12/Blogs-Api',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 7,
-        name: '',
-        stacks: ['', ''],
-        reposit: '',
-        imagem: '',
+        name: 'Trybe Futebol Club',
+        stacks: [
+          'TypeScript',
+          'Node',
+          'Express',
+          'JWT',
+          'Docker',
+          'MySQL',
+          'Sequelize',
+        ],
+        deploy: 'https://github.com/BenHurAlbertassi12/TrybeFutebolClub',
+        reposit: 'https://github.com/BenHurAlbertassi12/TrybeFutebolClub',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 8,
-        name: '',
-        stacks: ['', ''],
-        reposit: '',
-        imagem: '',
+        name: 'Sistem Solar',
+        stacks: ['React', 'Redux', 'HTML & CSS'],
+        deploy: 'https://github.com/BenHurAlbertassi12/SolarSystem',
+        reposit: 'https://github.com/BenHurAlbertassi12/SolarSystem',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 9,
-        name: '',
-        stacks: ['', ''],
-        reposit: '',
-        imagem: '',
+        name: 'TryUnfo',
+        stacks: ['JavaScript', 'HTML & CSS', 'React', 'Redux'],
+        deploy: 'https://github.com/BenHurAlbertassi12/TryUnfo',
+        reposit: 'https://github.com/BenHurAlbertassi12/TryUnfo',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
       {
         id: 10,
-        name: '',
-        stacks: [''],
-        reposit: '',
-        imagem: '',
+        name: 'Store Manager',
+        stacks: ['MySQL', 'Docker', 'TDD', 'MSC', 'RESTFul'],
+        deploy: 'https://github.com/BenHurAlbertassi12/StoreManager',
+        reposit: 'https://github.com/BenHurAlbertassi12/StoreManager',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
+      },
+      {
+        id: 12,
+        name: 'carShop',
+        stacks: ['TypeScript', 'POO', 'MongoDB'],
+        deploy:
+          'https://github.com/BenHurAlbertassi12/carShop/blob/main/src/Controllers/MotocycleController.ts',
+        reposit:
+          'https://github.com/BenHurAlbertassi12/carShop/blob/main/src/Controllers/MotocycleController.ts',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
+      },
+      {
+        id: 13,
+        name: 'TrybeWallet',
+        stacks: ['Redux', 'JavaScript', 'HTML & CSS', 'React'],
+        deploy: 'https://github.com/BenHurAlbertassi12/Wallet',
+        reposit: 'https://github.com/BenHurAlbertassi12/Wallet',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
+      },
+      {
+        id: 14,
+        name: 'Talker Manager',
+        stacks: ['Node', 'CRUD', 'API', 'MySQL'],
+        deploy: 'https://github.com/BenHurAlbertassi12/talkerManager',
+        reposit: 'https://github.com/BenHurAlbertassi12/talkerManager',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
+      },
+      {
+        id: 15,
+        name: 'Shopping Cart',
+        stacks: ['JavaScript', 'DOM'],
+        deploy: 'https://github.com/BenHurAlbertassi12/shopping-cart',
+        reposit: 'https://github.com/BenHurAlbertassi12/shopping-cart',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
+      },
+      {
+        id: 16,
+        name: 'TrybeSmith',
+        stacks: ['TypeScript', 'POO'],
+        deploy: 'https://github.com/BenHurAlbertassi12/trybeSmith',
+        reposit: 'https://github.com/BenHurAlbertassi12/trybeSmith',
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
     ],
     recomendations: [
       {
         id: 1,
-        name: '',
-        testemunhals: '',
-        imagem: '',
+        name: 'Alissa Maximo',
+        testemunhals: `
+            Estávamos no mesmo grupo, junto com outras pessoas, para realizar um projeto web de aplicativo simulando delivery;
+
+            Iniciamos bem o trabalho com paciência e combinados de grupo, cada um dando sua opinião e levando em consideração - tudo respeitosamente.
+
+            O BenHur tomou iniciativa em realizar uma boa porção de requisitos necessários, avançando bem o código; além disso, quando tinha dúvidas, procurava-nos por ajuda, e vice-versa ajudando a mim e outres, mostrando proatividade!
+
+            Foi ótimo trabalhar com o BenHur Albertasi!
+            `,
+        imagem:
+          'https://www.argali.com.br/wp-content/uploads/woocommerce-placeholder.png',
       },
     ],
   },
