c510cdf510a707e66529f629531c869d6e9f9d91
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-12 18:39:36 -0300

    Finalização aba de Portifólio

--- src/app/components/portfoil/portfoil.component.ts
+++ src/app/components/portfoil/portfoil.component.ts
@@ -9,12 +9,35 @@ import about from '../../data/about.json';
 export class PortfoilComponent implements OnInit {
   projects: any[] = [];
   selectedProject: any;
+  allStacks: string[] = [];
 
   ngOnInit(): void {
     this.projects = about.benhur[0].project;
+    this.getAllStacks();
   }
 
   selectProject(project: any): void {
     this.selectedProject = project;
+  }
+
+  getAllStacks(): void {
+    this.projects.forEach((project) => {
+      project.stacks.forEach((stack: string) => {
+        if (!this.allStacks.includes(stack)) {
+          this.allStacks.push(stack);
+        }
+      });
+    });
+  }
+
+  filterProjectsByStack(stack: string): void {
+    this.selectedProject = null; 
+    if (stack === 'all') {
+      this.projects = about.benhur[0].project; 
+    } else {
+      this.projects = about.benhur[0].project.filter((project) =>
+        project.stacks.includes(stack)
+      ); 
+    }
   }
 }
