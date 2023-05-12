17449b7748ae87768ed323bc00b315f6dcfba619
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-12 00:22:22 -0300

    json

--- src/app/components/portfoil/portfoil.component.ts
+++ src/app/components/portfoil/portfoil.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import about from '../../data/about.json'
 
 @Component({
   selector: 'app-portfoil',
   templateUrl: './portfoil.component.html',
   styleUrls: ['./portfoil.component.css']
 })
-export class PortfoilComponent {
+export class PortfoilComponent implements OnInit{
+  
+  title: string = ''
+  
+  ngOnInit(): void {
+    this.title = about.benhur[0].name
+
+    
+  }
 
 }
