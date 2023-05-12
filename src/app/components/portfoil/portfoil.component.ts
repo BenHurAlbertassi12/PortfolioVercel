43b0a1ad06872a07fce464ef2bab821df6ca1c9e
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-12 17:01:41 -0300

    Dinamic Projects

--- src/app/components/portfoil/portfoil.component.ts
+++ src/app/components/portfoil/portfoil.component.ts
@@ -1,19 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import about from '../../data/about.json'
+import about from '../../data/about.json';
 
 @Component({
   selector: 'app-portfoil',
   templateUrl: './portfoil.component.html',
-  styleUrls: ['./portfoil.component.css']
+  styleUrls: ['./portfoil.component.css'],
 })
-export class PortfoilComponent implements OnInit{
-  
-  title: string = ''
-  
-  ngOnInit(): void {
-    this.title = about.benhur[0].name
+export class PortfoilComponent implements OnInit {
+  projects: any[] = [];
+  selectedProject: any;
 
-    
+  ngOnInit(): void {
+    this.projects = about.benhur[0].project;
   }
 
+  selectProject(project: any): void {
+    this.selectedProject = project;
+  }
 }
