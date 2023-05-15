9ccbc46cf0c2fe287e3cae25bd8d4a0341af2666
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-15 19:57:49 -0300

    .

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import about from '../../data/about.json';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  courses: any[] = [];
 
+  ngOnInit(): void {
+    this.courses = about.benhur[0].courses.map((course: any) => ({
+      name: course.name,
+      course: course.course,
+      stacks: course.stacks,
+    }));
+  }
 }
