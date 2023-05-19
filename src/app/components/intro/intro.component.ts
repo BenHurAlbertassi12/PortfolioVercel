0de7532e4f4febe57675491b28d90983b3dbc424
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-05-19 14:41:01 -0300

    css

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import about from '../../data/about.json';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.css']
+  styleUrls: ['./intro.component.css'],
 })
-export class IntroComponent {
+export class IntroComponent implements OnInit {
+
+  name:string = ''
+  title:string = ''
+  text:string = ''
+
+
+  ngOnInit(): void {
+    this.name = about.benhur[0].name
+    this.title = about.benhur[0].function;
+    this.text = about.benhur[0].description;
+  }
 
 }
